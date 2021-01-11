dd2a5bb2d43a872a1b30a88477068b0e22942256
Lev Stambler <levstamb@gmail.com>
2021-01-11 17:04:24 -0500

    add user protection

--- contracts/SiteStore.sol
+++ contracts/SiteStore.sol
@@ -1,21 +1,31 @@
 pragma solidity ^0.7.0;
 
 contract SiteStore {
-    mapping(string => string) public ipfsDNS;
+    struct Site {
+        string ipfsCID;
+        address owner;
+    }
+
+    mapping(string => Site) public ipfsDNS;
     mapping(string => bool) public dnsInUse;
 
     constructor() {}
 
-    function addSiteToDNS(string memory name, string memory ipfsPath) public {
+    function addSiteToDNS(string memory name, string memory ipfsCID) public {
+        require(msg.sender != address(0), "An address is required to be send with the messege");
         require(dnsInUse[name] == false, "This DNS name is already in use");
-        ipfsDNS[name] = ipfsPath;
+        ipfsDNS[name] = Site(ipfsCID, msg.sender);
         dnsInUse[name] = true;
     }
 
     function deleteSiteDNS(string memory name) public {
+        require(
+            msg.sender != address(0) && ipfsDNS[name].owner == msg.sender,
+            "You can only delete sites which you own"
+        );
         if (dnsInUse[name]) {
             dnsInUse[name] = false;
-            ipfsDNS[name] = "";
+            ipfsDNS[name] = Site("", address(0));
         }
     }
 }
--- test/sitestore.spec.js
+++ test/sitestore.spec.js
@@ -1,15 +1,45 @@
+require("chai").use(require("chai-as-promised")).should();
 const SiteStore = artifacts.require("./SiteStore.sol");
 
 contract("SiteStore", (accounts) => {
+  let person1, person2;
+  before(async () => {
+    person1 = accounts[0];
+    person2 = accounts[1];
+  });
+
   it("should put some domains onto the DNS and then remove them", async () => {
     const siteStore = await SiteStore.new();
-    await siteStore.addSiteToDNS("DDDD", "AAAA");
+
+    await siteStore.addSiteToDNS("DDDD", "AAAA", { from: person1 });
+
     let res = await siteStore.dnsInUse("DDDD");
     assert.equal(res.toString(), "true");
+
     res = await siteStore.ipfsDNS("DDDD");
-    assert.equal(res.toString(), "AAAA");
-    await siteStore.deleteSiteDNS("DDDD");
+    assert.equal(res.ipfsCID.toString(), "AAAA");
+
+    await siteStore.deleteSiteDNS("DDDD", { from: person1 });
+
     res = await siteStore.dnsInUse("DDDD");
     assert.equal(res.toString(), "false");
+  });
+  it("should put some domains onto the DNS and then add the same one and fail", async () => {
+    const siteStore = await SiteStore.new();
+    await siteStore.addSiteToDNS("DDDD", "AAAA", { from: person1 });
+    let res = await siteStore.dnsInUse("DDDD");
+    assert.equal(res.toString(), "true");
+    await siteStore.addSiteToDNS("DDDD", "AAAA", { from: person1 }).should.be
+      .rejected;
+  });
+  it("should reject someone trying to delete a site which they do not owner", async () => {
+    const siteStore = await SiteStore.new();
+
+    await siteStore.addSiteToDNS("DDDD", "AAAA", { from: person1 });
+
+    let res = await siteStore.dnsInUse("DDDD");
+    assert.equal(res.toString(), "true");
+
+    await siteStore.deleteSiteDNS("DDDD", { from: person2 }).should.be.rejected;
   });
 });
